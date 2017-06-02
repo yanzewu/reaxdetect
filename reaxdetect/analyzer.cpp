15cc1d71bbf6e2c605259a762e6c1f5a7a206f8b
flipboards <wuyanze0@gmail.com>
2017-06-02 12:57:27 +0800

    bug fix in analyzer output

--- reaxdetect/analyzer.cpp
+++ reaxdetect/analyzer.cpp
@@ -43,7 +43,7 @@ void ReaxAnalyzer::HandleData(const ReaxReader& reader, const Simulation& simula
 		tsize_t sample_int = (tsize_t)(config.sample_int / simulation.timeStep);
 		tsize_t sample_range = (tsize_t)(config.sample_range / simulation.timeStep);
 
-		printf("Using fixed sample with interval %f(%d), range %f(%d)\n", sample_int, sample_range);
+		printf("Using fixed sample with interval %f(%d), range %f(%d)\n", config.sample_int, sample_int, config.sample_range, sample_range);
 		FixSample(reader, config.sample_int, config.sample_range, interval, simulation.volume);
 	}
 	else {
